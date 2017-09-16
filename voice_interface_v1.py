355da1ce59366eaa56083e3d4e6d29f448b33829
Eric B <eric.bezzam@epfl.ch>
2017-09-16 22:30:35 +0200

    Voice interface v2.

--- voice_interface_v1.py
+++ voice_interface_v1.py
@@ -1,72 +1,14 @@
 from __future__ import print_function
 import sounddevice as sd
 import soundfile as sf
-import requests
-import httplib
-import uuid
-import json
 import os
+from utils import *
+
+from bingtts import Translator
 
 SAMPLERATE = 16000
 NUMCHANNELS = 1
-DURATION = 8
-
-
-class Microsoft_ASR():
-    def __init__(self):
-        self.sub_key = os.environ['MICROSOFT_VOICE']
-        self.token = None
-        pass
-
-    def get_speech_token(self):
-        FetchTokenURI = "/sts/v1.0/issueToken"
-        header = {'Ocp-Apim-Subscription-Key': self.sub_key}
-        conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
-        body = ""
-        conn.request("POST", FetchTokenURI, body, header)
-        response = conn.getresponse()
-        str_data = response.read()
-        conn.close()
-        self.token = str_data
-        print("Got Token: ", self.token)
-        return True
-
-    def transcribe(self,speech_file):
-
-        # Grab the token if we need it
-        if self.token is None:
-            print("No Token... Getting one")
-            self.get_speech_token()
-
-        endpoint = 'https://speech.platform.bing.com/recognize'
-        request_id = uuid.uuid4()
-        # Params form Microsoft Example 
-        params = {'scenarios': 'ulm',
-                  'appid': 'D4D52672-91D7-4C74-8AD8-42B1D98141A5',
-                  'locale': 'en-US',
-                  'version': '3.0',
-                  'format': 'json',
-                  'instanceid': '565D69FF-E928-4B7E-87DA-9A750B96D9E3',
-                  'requestid': uuid.uuid4(),
-                  'device.os': 'linux'}
-        content_type = "audio/wav; codec=""audio/pcm""; samplerate=16000"
-
-        def stream_audio_file(speech_file, chunk_size=1024):
-            with open(speech_file, 'rb') as f:
-                while 1:
-                    data = f.read(1024)
-                    if not data:
-                        break
-                    yield data
-
-        headers = {'Authorization': 'Bearer ' + self.token, 
-                   'Content-Type': content_type}
-        resp = requests.post(endpoint, 
-                            params=params, 
-                            data=stream_audio_file(speech_file), 
-                            headers=headers)
-        val = json.loads(resp.text)
-        return val["results"][0]["name"], val["results"][0]["confidence"]
+DURATION = 10
 
 
 def speak_to_YuMi():
@@ -99,11 +41,18 @@ def speak_to_YuMi():
         print("I'm sorry, I didn't quite get that")
 
 
+    output = translator.speak("This is a text to speech translation", "en-US", "Female", "riff-16khz-16bit-mono-pcm")
+
+
+
 if __name__ == "__main__":
 
-    # initialize Microsoft ASR
-    ms_asr = Microsoft_ASR()
+    # initialize Microsoft ASR and Text2Speech
+    api_key = os.environ['MICROSOFT_VOICE']
+    ms_asr = Microsoft_ASR(api_key)
     ms_asr.get_speech_token()
+    translator = Translator(api_key)
+
 
     sd.default.samplerate = SAMPLERATE
     sd.default.channels = NUMCHANNELS
