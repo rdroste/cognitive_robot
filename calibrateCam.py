4f5279b8ec0fbf79da9f07b0d0a7311bbc0e4499
Richard <droste.richard@gmail.com>
2017-09-17 02:20:04 +0200

    somethoi

--- calibrateCam.py
+++ calibrateCam.py
@@ -1,21 +1,27 @@
 import numpy as np
 import cv2
+import utils
+import time
 
 # Main routine
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
-    # Check Pegboard
-    cap = utils.prepare_camera()
-    ret, frame = cap.read()
-    frame = frame[250:350, 230:470]
-    cv2.imshow('image', frame)
-    cv2.waitKey()
+# Check Pegboard
+cv2.destroyAllWindows()
 
-    # Check Coin
-    ret, frame = cap.read()
-    roi_coords = np.array([[282, 349], [219, 282]])
-    frame = frame[roi_coords[0, 0]:roi_coords[0, 1], roi_coords[1, 0]:roi_coords[1, 1]]
-    cv2.imshow('image', frame)
-    cv2.waitKey()
-
-    utils.close_camera(cap)
+cap = utils.prepare_camera(3)
+time.sleep(2)
+ret, frame = cap.read()
+time.sleep(2)
+frame = frame[370:460, 240:400]
+cv2.imshow('image', frame)
+cv2.waitKey()
+#
+# # Check Coin
+# ret, frame = cap.read()
+# roi_coords = np.array([[282, 349], [219, 282]])
+# frame = frame[roi_coords[0, 0]:roi_coords[0, 1], roi_coords[1, 0]:roi_coords[1, 1]]
+# cv2.imshow('image', frame)
+# cv2.waitKey()
+#
+utils.close_camera(cap)
